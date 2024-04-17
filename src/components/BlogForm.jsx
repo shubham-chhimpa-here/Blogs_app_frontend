303b5d578a23d70cf1932d39126adb6938643d37
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-04-17 06:22:29 +0530

    submit

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -15,6 +15,13 @@ export default function BlogForm() {
 				'content-type': 'application/json'
 			}
 		})
+		.then(res => res.json())
+		.then(res => {
+			console.log(res)
+		})
+		.catch(err => {
+			console.log(err)
+		})
 
 	}
 
